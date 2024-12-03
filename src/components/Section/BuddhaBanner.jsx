21f5538c824a3fd230e42ce20512598cd53694f5
SaikatDev <saikat.b965@gmail.com>
2024-12-02 19:39:01 -0500

    Added changes with animation

--- src/components/Section/BuddhaBanner.jsx
+++ src/components/Section/BuddhaBanner.jsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function BuddhaBanner() {
+  useEffect(() => {
+    // Scroll to the top of the page on component mount
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, []);
+
   return (
     <div id="home" className="relative">
+      {/* Title Section */}
+      <div className="absolute inset-0 flex items-end justify-center z-10 animate-titleFadeIn">
+        <div className="flex flex-col items-center">
+          <div className="text-center text-white">
+            <h1 className="text-6xl font-bold">
+              Brahma Vihara Meditation Center
+            </h1>
+            <p className="text-lg mt-2">123 Meditation Way, Peaceful City</p>
+          </div>
+          <div>
+            <button className="text-white text-bold text-xl bg-orange-500 px-10 py-3 mt-10 rounded-xl hover:bg-orange-400">
+              PLAN YOUR VISIT
+            </button>
+          </div>
+        </div>
+      </div>
+
       {/* Scrollable Banner */}
       <div className="h-screen w-full overflow-y-scroll">
         <img
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import shadows from "@mui/material/styles/shadows";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -6,15 +8,25 @@ export default {
       fontFamily: {
         roboto: ["Roboto", "sans-serif"], // Define 'font-roboto'
         lobster: ["Lobster", "cursive"], // Define 'font-lobster'
-        // Add more fonts as needed
       },
       animation: {
         fadeIn: "fadeIn 0.5s ease-in-out forwards",
+        titleFadeIn: "titleFadeIn 3s ease-out forwards", // New animation for title
       },
       keyframes: {
         fadeIn: {
           "75%": { opacity: "0" },
           "100%": { opacity: "1" },
+        },
+        titleFadeIn: {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-25%)", // Start slightly below the center
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(-35%)", // End at slightly above the center
+          },
         },
       },
     },
